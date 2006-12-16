d57f693343e5ac641c965ef580faca30173ed832
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2006-12-16 10:47:31 +0000

    Added the osm directory.
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@25 25d90789-57f7-4ee0-8453-03a3dfeeeb22

---
+++ src/uk/me/parabola/mkosmgmap/osm/ReadOsm.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2006 Steve Ratcliffe
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ * 
+ * Author: Steve Ratcliffe
+ * Create date: 16-Dec-2006
+ */
+package uk.me.parabola.mkosmgmap.osm;
+
+import uk.me.parabola.mkosmgmap.MapSource;
+
+/**
+ * @author Steve Ratcliffe
+ */
+public class ReadOsm implements MapSource {
+}
