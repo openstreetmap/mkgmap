e35ed7b74ca5fe44135b6c4f9b13f03ffa4eec52
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2007-02-08 20:46:11 +0000

    May as well remove concurrent hash map it will then run with gcj.  Its ten times slower though.
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@147 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/log/Logger.java
+++ src/uk/me/parabola/log/Logger.java
@@ -18,9 +18,9 @@ package uk.me.parabola.log;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.HashMap;
 
 
 /**
@@ -40,7 +40,7 @@ public class Logger {
 	private static boolean loggingActive;
 
 	private static final Map<String, Logger> loggers
-			= new ConcurrentHashMap<String, Logger>();
+			= new HashMap<String, Logger>();
 
 	private String shortname;
 
