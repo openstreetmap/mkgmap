5a32901675447253462bb0cf8b41387734b5c5fa
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2007-09-02 12:49:48 +0000

    The so-called 10-bit encoder, spoils the package dependancies and is probably not even correct.  I'd imagine that they would just use unicode for other international character sets.
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@212 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/imgfmt/app/labelenc/Bit10Encoder.java
+++ src/uk/me/parabola/imgfmt/app/labelenc/Bit10Encoder.java
@@ -16,33 +16,34 @@
  */
 package uk.me.parabola.imgfmt.app.labelenc;
 
-import uk.me.parabola.imgfmt.app.BitWriter;
-
 /**
  * Very experimental.  In fact no evidence that the encoding length
  * of 10 actually means 10 bit format.  At least on my Ledgend Cx it is
  * actually just the same as 8 bit.
+ *
+ * This will probably be removed.
  *
  * @author Steve Ratcliffe
  */
 public class Bit10Encoder extends BaseEncoder implements CharacterEncoder {
 
 	public EncodedText encodeText(String text) {
-		if (text == null)
-			return NO_TEXT;
-
-		BitWriter bw = new BitWriter();
-
-		String[] strings = text.split(",");
-		for (String s : strings) {
-			try {
-				int c = Integer.decode(s);
-				bw.putn(c, 10);
-			} catch (NumberFormatException e) {
-				bw.putn(0, 10);
-			}
-		}
-
-		return new EncodedText(bw.getBytes(), bw.getLength());
+		//if (text == null)
+		//	return NO_TEXT;
+		//
+		//BitWriter bw = new BitWriter();
+		//
+		//String[] strings = text.split(",");
+		//for (String s : strings) {
+		//	try {
+		//		int c = Integer.decode(s);
+		//		bw.putn(c, 10);
+		//	} catch (NumberFormatException e) {
+		//		bw.putn(0, 10);
+		//	}
+		//}
+		//
+		//return new EncodedText(bw.getBytes(), bw.getLength());
+		throw new RuntimeException("not implemented yet");
 	}
 }
