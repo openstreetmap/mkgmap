13f6f85eadc47f07c063167396e7d0c36c0bd9fa
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2009-02-14 13:44:57 +0000

    Output changed slightly due to previous fix. Test given correct number.
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@882 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- test/func/SimpleTest.java
+++ test/func/SimpleTest.java
@@ -103,7 +103,7 @@ public class SimpleTest {
 			int size = ent.getSize();
 			if (ext.equals("RGN")) {
 				count++;
-				assertEquals("RGN size", 2939, size);
+				assertEquals("RGN size", 2938, size);
 			} else if (ext.equals("TRE")) {
 				count++;
 				assertEquals("TRE size", 579, size);
