21a7cf334335c33f5ccef6dae0dd75d979113c6e
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2009-04-05 20:25:24 +0000

    Read style files in utf-8 always. -Ulf Lamping
    Note: the built in files will always be in utf-8 (if downloaded from the web site anyway).
    
    
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@999 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/mkgmap/osmstyle/DirectoryFileLoader.java
+++ src/uk/me/parabola/mkgmap/osmstyle/DirectoryFileLoader.java
@@ -20,6 +20,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.UnsupportedEncodingException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
@@ -53,7 +56,13 @@ public class DirectoryFileLoader extends StyleFileLoader {
 	 */
 	public Reader open(String filename) throws FileNotFoundException {
 		File file = new File(dir, filename);
-		Reader r = new FileReader(file);
+		
+		Reader r = null;
+		try {
+			r = new InputStreamReader(new FileInputStream(file), "UTF-8");
+        } catch (UnsupportedEncodingException uee) {
+            System.out.println("DirectoryFileLoader: Encoding UTF-8 not supported");
+        }
 
 		return new BufferedReader(r);
 	}
