e74fac5953ac5dfc925e0a04a401aeb82832d4a7
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2009-07-02 18:53:23 +0000

    Sends a message to the console from an action block.
    Useful to see what is happening when creating complex rules.
    
    - Richard Fairhurst
    
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@1074 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/mkgmap/osmstyle/actions/ActionReader.java
+++ src/uk/me/parabola/mkgmap/osmstyle/actions/ActionReader.java
@@ -66,6 +66,9 @@ public class ActionReader {
 				String to = scanner.nextWord();
 				Action act = new RenameAction(from, to);
 				actions.add(act);
+			} else if ("echo".equals(cmd)) {
+				String str = scanner.nextWord();
+				actions.add(new EchoAction(str));
 			} else {
 				throw new SyntaxException(scanner, "Unrecognised command '" + cmd + '\'');
 			}
