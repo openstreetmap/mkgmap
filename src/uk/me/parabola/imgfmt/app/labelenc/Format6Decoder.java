4633d7ebfc53c4dac3a45c948b7f1dbdc84403a4
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2009-09-26 23:48:27 +0000

    The format 6 decoder was not correctly setting after the end of a string.  You have to skip to a byte boundary from the beginning of the end market - this can be less than 6 bits (or more).
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/branches/mdr@1233 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/imgfmt/app/labelenc/Format6Decoder.java
+++ src/uk/me/parabola/imgfmt/app/labelenc/Format6Decoder.java
@@ -11,8 +11,8 @@ public class Format6Decoder implements CharacterDecoder {
 
 	private boolean symbol;
 
-	private short store;
-	private int offset = -6;
+	private int store;
+	private int nbits;
 
 	public boolean addByte(int in) {
 		int b = 0xff & in; //wipe out high bits (in case of negative byte)
@@ -20,27 +20,36 @@ public class Format6Decoder implements CharacterDecoder {
 		if (needReset) {
 			needReset = false;
 			out.reset();
-			store = 0;
-			offset = -6;
 		}
 
 		store <<= 8;
 		store |= b;
-		offset += 8;
+		nbits += 8;
 
-		addChar((store >> offset) & 0x3f);
-		offset -= 6;
+		while (nbits >= 6) {
+			convertChar((store >> (nbits-6)) & 0x3f);
 
-		if (offset >= 0 && !needReset) {
-			addChar((store >> offset) & 0x3f);
-			offset -= 6;
+			if (needReset) {
+				// Skip until the next byte boundary.  Note that may mean that
+				// we skip more or *less* than 6 bits.
+				if (nbits > 8)
+					nbits = 8;
+				else
+					nbits = 0;
+
+				break;
+			} else
+				nbits -= 6;
 		}
-		
+
 		return needReset;
 	}
 
-	private void addChar(int b) {
-
+	/**
+	 * Convert a single 6 bit quantity into a character.
+	 * @param b The six bit int.
+	 */
+	private void convertChar(int b) {
 		if (b > 0x2f) {
 			needReset = true;
 			return;
@@ -53,20 +62,21 @@ public class Format6Decoder implements CharacterDecoder {
 			c = Format6Encoder.SYMBOLS.charAt(b);
 		} else {
 			switch(b) {
-				case 0x1B:
-					// perhaps this is "next-char lower case"?
-					return;
-				case 0x1C:
-					// next char is symbol
-					symbol = true;
-					return;
-				case 0x1D:
-				case 0x1E:
-				case 0x1F:
-					// these define abbreviations; fall through to
-					// lookup which returns a space
-				default:
-					c = Format6Encoder.LETTERS.charAt(b);
+			case 0x1B:
+				// perhaps this is "next-char lower case"?
+				return;
+			case 0x1C:
+				// next char is symbol
+				symbol = true;
+				return;
+			case 0x1D:
+			case 0x1E:
+			case 0x1F:
+				// these define abbreviations; fall through to
+				// lookup which returns a space
+			default:
+				c = Format6Encoder.LETTERS.charAt(b);
+				break;
 			}
 		}
 		out.write(c);
