9e6a49c7f0b90c94ccf43b6fe9e8416338a62c86
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2009-11-12 17:36:59 +0000

    Documentation updates -Greg Troxel
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@1381 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- doc/README.java
+++ doc/README.java
@@ -6,6 +6,16 @@ README.java for mkgmap
 
 Version 1.6, or later, of java is required to run mkgmap.
 
+** Mac hints
+
+Mac OS X 10.5 comes with Java 1.5.  You can install 1.6 from
+  http://www.apple.com/downloads/macosx/apple/application_updates/javaformacosx105update1.html
+
+After installing, set 1.6 as the default using JAVA preferences, which
+will cause 'java -jar foo.jar' to use the 1.6 JRE.  To cause ant to
+use 1.6 to compile, set
+  export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/home
+
 * building
 
 To build, you must have a JDK and apache ant installed.
@@ -14,4 +24,4 @@ Then, just run "ant", which will produce dist/mkgmap.jar.
 * running
 
 To run, you must have a JRE installed.  See README.invoking and
-README.examples.
+README.examples.  You can use a downloaded jar or one that you built.
