openstreetmap/mkgmap

commit 542e0d44c60294d5bd4676f83c7975e7a41e94ca

Duplicate ways before calling resolveType().
This will stop any changes to the way by either the style rule actions or
the map object generation affecting other map objects generated from the
same way.

git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@1412 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/mkgmap/osmstyle/StyledConverter.java
@@ -277,20 +277,21 @@ public class StyledConverter implements OsmConverter {
 			preConvertRules(way);
 
 			do {
-				foundType = wayRules.resolveType(way, foundType);
+				Way dupWay = way.duplicate();
+				foundType = wayRules.resolveType(dupWay, foundType);
 				if (foundType == null)
 					return;
 
-				postConvertRules(way, foundType);
+				postConvertRules(dupWay, foundType);
 
 				if (foundType.getFeatureKind() == GType.POLYLINE) {
 					if(foundType.isRoad())
-						addRoad(way, foundType);
+						addRoad(dupWay, foundType);
 					else
-						addLine(way, foundType);
+						addLine(dupWay, foundType);
 				}
 				else
-					addShape(way, foundType);
+					addShape(dupWay, foundType);
 			} while (!foundType.isFinal());
 		}
 	}

commit 0cd340340a35043e960401d0e8ec372f13e7c294

Fix turn restriction breakage introduced by r1412.
Duplicating the way meant that it would no longer be matched as a turn
restriction 'from' or 'to' way. Associating the duplicate to the
original way with an entry in originalWay fixes the bug.

git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@1414 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/mkgmap/osmstyle/StyledConverter.java
@@ -282,6 +282,7 @@ public class StyledConverter implements OsmConverter {
 				if (foundType == null)
 					return;
 
+				originalWay.put(dupWay, way);
 				postConvertRules(dupWay, foundType);
 
 				if (foundType.getFeatureKind() == GType.POLYLINE) {

src/uk/me/parabola/mkgmap/reader/osm/RestrictionRelation.java
@@ -273,7 +273,7 @@ public class RestrictionRelation extends Relation {
 
 		if(restriction == null || viaNode == null || fromNode == null || toNode == null) {
 			// restriction must have some error (reported earlier)
-		    return;
+			return;
 		}
 
 		if(restriction.equals("no_left_turn") ||
@@ -294,8 +294,10 @@ public class RestrictionRelation extends Relation {
 			if(restriction.startsWith("only_turn"))
 				log.warn(messagePrefix + "has bad type '" + restriction + "' it should be of the form only_X_turn rather than only_turn_X - I added the restriction anyway - allows routing to way " + toWay.toBrowseURL());
 			log.info(messagePrefix + restriction + " added - allows routing to way " + toWay.toBrowseURL());
-			for(CoordNode otherNode : otherNodes)
+			for(CoordNode otherNode : otherNodes) {
+				log.info(messagePrefix + restriction + "  blocks routing to node " + otherNode.toOSMURL());
 				roadNetwork.addRestriction(fromNode, otherNode, viaNode, exceptMask);
+			}
 		}
 		else {
 			log.warn(messagePrefix + "has unsupported type '" + restriction + "'");