openstreetmap/mkgmap

commit a77ba1020147cf389e4d704a50e704136505d611

Added support for "carpool lanes".
Any way can now be flagged as a "carpool lane" by giving it a
carpool tag (e.g. carpool=yes). If the GPS is set to avoid
carpool lanes, the way should only be routed through if the
vehicle type is emergency or bus. If the GPS is not set to avoid
carpool lanes, the way will be used by any vehicle type.

Note that the normal access tags (e.g. motorcar=no) cannot be used at
the same time as carpool=yes and they will be ignored.

git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@1419 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/imgfmt/app/net/RoadDef.java
@@ -30,6 +30,8 @@ import uk.me.parabola.imgfmt.app.lbl.Zip;
 import uk.me.parabola.imgfmt.app.trergn.Polyline;
 import uk.me.parabola.log.Logger;
 
+import uk.me.parabola.mkgmap.general.RoadNetwork;
+
 /**
  * A road definition.  This ties together all segments of a single road
  * and provides street address information.
@@ -378,9 +380,8 @@ public class RoadDef implements Comparable {
 	// number of nodes in the road
 	private int nnodes;
 
-	public static final int NOD2_MASK_SPEED = 0x0e;
-	public static final int NOD2_MASK_CLASS = 0xf0; // might be less
-	private static final int NOD2_FLAG_UNK = 0x01;
+	private static final int NOD2_FLAG_UNK        = 0x01;
+	private static final int NOD2_FLAG_EXTRA_DATA = 0x80;
 
 	// always appears to be set
 	private int nod2Flags = NOD2_FLAG_UNK;
@@ -483,6 +484,7 @@ public class RoadDef implements Comparable {
 		0x0001, // car
 		0x0002, // bus
 		0x0004, // taxi
+		0x0008, // carpool
 		0x0010, // foot
 		0x0020, // bike
 		0x0040, // truck
@@ -496,16 +498,12 @@ public class RoadDef implements Comparable {
 		tabAInfo |= TABA_FLAG_TOLL;
 	}
 
-	public void setUnknownAccess08() {
-		tabAAccess |= 0x08;
-	}
-
 	public void setNoThroughRouting() {
 		tabAAccess |= 0x80;
 	}
 
 	public void setAccess(boolean[] access) {
-		for (int i = 0; i < 8; i++)
+		for (int i = 0; i < RoadNetwork.NO_MAX; i++)
 			if (access[i])
 				tabAAccess |= ACCESS[i];
 	}

src/uk/me/parabola/mkgmap/general/RoadNetwork.java
@@ -47,10 +47,11 @@ public class RoadNetwork {
 	public static final int NO_CAR = 2;
 	public static final int NO_BUS = 3;
 	public static final int NO_TAXI = 4;
-	public static final int NO_FOOT = 5;
-	public static final int NO_BIKE = 6;
-	public static final int NO_TRUCK = 7;
-	public static final int NO_MAX = 8;
+	public static final int NO_CARPOOL = 5;
+	public static final int NO_FOOT = 6;
+	public static final int NO_BIKE = 7;
+	public static final int NO_TRUCK = 8;
+	public static final int NO_MAX = 9;
 
 	private final Map<Long, RouteNode> nodes = new LinkedHashMap<Long, RouteNode>();
 

src/uk/me/parabola/mkgmap/general/MapRoad.java
@@ -85,10 +85,6 @@ public class MapRoad extends MapLine {
 		this.roadDef.setAccess(access);
 	}
 
-	public void setUnknownAccess08() {
-		this.roadDef.setUnknownAccess08();
-	}
-
 	public void setNoThroughRouting() {
 		this.roadDef.setNoThroughRouting();
 	}

src/uk/me/parabola/mkgmap/osmstyle/StyledConverter.java
@@ -124,6 +124,7 @@ public class StyledConverter implements OsmConverter {
 	private final AccessMapping[] accessMap = {
 		new AccessMapping("access",     RoadNetwork.NO_MAX), // must be first in list
 		new AccessMapping("bicycle",    RoadNetwork.NO_BIKE),
+		new AccessMapping("carpool",    RoadNetwork.NO_CARPOOL),
 		new AccessMapping("foot",       RoadNetwork.NO_FOOT),
 		new AccessMapping("hgv",        RoadNetwork.NO_TRUCK),
 		new AccessMapping("motorcar",   RoadNetwork.NO_CAR),
@@ -1406,6 +1407,18 @@ public class StyledConverter implements OsmConverter {
 			} else {
 				log.info("Ignoring unsupported access tag value " + type + "=" + accessTagValue + " in " + highwayType + " " + debugWayName);
 			}
+		}
+
+		if(way.isBoolTag("carpool")) {
+			// to make a way into a "carpool lane" all access disable
+			// bits must be set except for CARPOOL and EMERGENCY (BUS
+			// can also be clear)
+			road.setNoThroughRouting();
+			for (int j = 1; j < accessMap.length; ++j)
+				noAccess[accessMap[j].index] = true;
+			noAccess[RoadNetwork.NO_CARPOOL] = false;
+			noAccess[RoadNetwork.NO_EMERGENCY] = false;
+			noAccess[RoadNetwork.NO_BUS] = false;
 		}
 
 		road.setAccess(noAccess);

commit 152c74969c446ee0e3b8b533864e4f60704cb275

Fix regression introduced in r1419.
Oops, inserting a new value in the middle of RoadDef.ACCESS[] was the
wrong thing to do. Moved it to the end.

git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@1424 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/imgfmt/app/net/RoadDef.java
@@ -484,10 +484,10 @@ public class RoadDef implements Comparable {
 		0x0001, // car
 		0x0002, // bus
 		0x0004, // taxi
-		0x0008, // carpool
 		0x0010, // foot
 		0x0020, // bike
 		0x0040, // truck
+		0x0008, // carpool
 	};
 
 	// The data for Table A

src/uk/me/parabola/mkgmap/general/RoadNetwork.java
@@ -47,10 +47,10 @@ public class RoadNetwork {
 	public static final int NO_CAR = 2;
 	public static final int NO_BUS = 3;
 	public static final int NO_TAXI = 4;
-	public static final int NO_CARPOOL = 5;
-	public static final int NO_FOOT = 6;
-	public static final int NO_BIKE = 7;
-	public static final int NO_TRUCK = 8;
+	public static final int NO_FOOT = 5;
+	public static final int NO_BIKE = 6;
+	public static final int NO_TRUCK = 7;
+	public static final int NO_CARPOOL = 8;
 	public static final int NO_MAX = 9;
 
 	private final Map<Long, RouteNode> nodes = new LinkedHashMap<Long, RouteNode>();