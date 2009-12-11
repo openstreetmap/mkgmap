openstreetmap/mkgmap

commit 5385c0d086fa3c2875a4c9993025cf868428efca

Avoid assertion when splitting way at a previous node.
When a way is split to avoid exceeding the maximum number of points in
a way and the split is done at a previous node, set a flag so that the
later code that checks for duplicate node indices doesn't barf because
the node index has already been seen before. In fact, in this situation,
we can reverse the check such that it requires the node index to have
been seen before and it will barf if it hasn't.

git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@1435 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/mkgmap/osmstyle/StyledConverter.java
@@ -1105,6 +1105,10 @@ public class StyledConverter implements OsmConverter {
 		for(int i = 0; i < points.size(); ++i) {
 			Coord p = points.get(i);
 
+			// flag that's set true when we back up to a previous node
+			// while finding a good place to split the line
+			boolean splitAtPreviousNode = false;
+
 			wayBBox.addPoint(p);
 
 			// check if we should split the way at this point to limit
@@ -1197,6 +1201,9 @@ public class StyledConverter implements OsmConverter {
 						// points so the loop will now terminate
 						p = points.get(splitI);
 						i = splitI; // hack alert! modify loop index
+						// note that we have split the line at a node
+						// that has already been processed
+						splitAtPreviousNode = true;
 					}
 					else if(splitI > 0) {
 						log.info("Splitting way " + debugWayName + " at " + points.get(splitI).toOSMURL() + " (making a new node) to limit number of points in this way to " + (splitI + 1) + ", way has " + (points.size() - splitI) + " more points");
@@ -1231,8 +1238,18 @@ public class StyledConverter implements OsmConverter {
 					// assign a node id
 					nodeIdMap.put(p, nextNodeId++);
 				}
-				assert !nodeIndices.contains(i) : debugWayName + " has multiple nodes for point " + i;
-				nodeIndices.add(i);
+
+				if(splitAtPreviousNode) {
+					// consistency check - this node index should
+					// already be recorded
+					assert nodeIndices.contains(i) : debugWayName + " has backed up to point " + i + " but can't find a node for that point " + p.toOSMURL();
+				}
+				else {
+					// add this index to nodeIndices (should not
+					// already be there)
+					assert !nodeIndices.contains(i) : debugWayName + " has multiple nodes for point " + i + " new node is " + p.toOSMURL();
+					nodeIndices.add(i);
+				}
 
 				if((i + 1) < points.size() &&
 				   nodeIndices.size() == MAX_NODES_IN_WAY) {

commit 23a7a8da9736a8be5b73bb4e898d2d858d8c1106

Fix regression introduced by r1435.
When scanning backwards looking for a place to split the way to limit
the way's number of points, don't set the flag that says we're splitting
the way at a previous node if the split is going to be at the current
point (i.e. we didn't need to go backwards at all).

git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@1436 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/mkgmap/osmstyle/StyledConverter.java
@@ -1192,10 +1192,19 @@ public class StyledConverter implements OsmConverter {
 						  points.get(splitI).getHighwayCount() < 2 &&
 						  !safeToSplitWay(points, splitI, i - numPointsInArc - 1, points.size() - 1))
 						--splitI;
-					if(points.get(splitI).getHighwayCount() > 1) {
+					if(points.get(i).getHighwayCount() > 1) {
+						// the current point is going to be a node
+						// anyway so split right here
+						log.info("Splitting way " + debugWayName + " at " + points.get(i).toOSMURL() + " (would be a node anyway) to limit number of points in this way to " + (i + 1) + ", way has " + (points.size() - i) + " more points");
+						assert trailingWay == null : "trailingWay not null #6a";
+						trailingWay = splitWayAt(way, i);
+						// this will have truncated the current Way's
+						// points so the loop will now terminate
+					}
+					else if(points.get(splitI).getHighwayCount() > 1) {
 						// we have found an existing node, use that
 						log.info("Splitting way " + debugWayName + " at " + points.get(splitI).toOSMURL() + " (using an existing node) to limit number of points in this way to " + (splitI + 1) + ", way has " + (points.size() - splitI) + " more points");
-						assert trailingWay == null : "trailingWay not null #6a";
+						assert trailingWay == null : "trailingWay not null #6b";
 						trailingWay = splitWayAt(way, splitI);
 						// this will have truncated the current Way's
 						// points so the loop will now terminate
@@ -1207,7 +1216,7 @@ public class StyledConverter implements OsmConverter {
 					}
 					else if(splitI > 0) {
 						log.info("Splitting way " + debugWayName + " at " + points.get(splitI).toOSMURL() + " (making a new node) to limit number of points in this way to " + (splitI + 1) + ", way has " + (points.size() - splitI) + " more points");
-						assert trailingWay == null : "trailingWay not null #6b";
+						assert trailingWay == null : "trailingWay not null #6c";
 						trailingWay = splitWayAt(way, splitI);
 						// this will have truncated the current Way's
 						// points so the loop will now terminate