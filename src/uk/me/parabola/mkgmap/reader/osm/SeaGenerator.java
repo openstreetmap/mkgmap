b0e8334b742afc576d2eaac529cffec94cd47de3
wanmil <wanmil@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2011-05-24 16:23:46 +0000

    Let LineSplitter return at most connected ways
    Ways are no longer split when two subsequent coordinates are equal.
    In case a way is closed the clipped ways are no longer splitted at its start/end point
    Slightly improved clip algorithm that now performs the most common check first if both points are within the clipping area
    
    As a result from these fixed the SeaGenerator no longer needs to join the clipped ways.
    
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@1954 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/mkgmap/reader/osm/SeaGenerator.java
+++ src/uk/me/parabola/mkgmap/reader/osm/SeaGenerator.java
@@ -453,23 +453,6 @@ public class SeaGenerator extends OsmReadingHooksAdaptor {
 			List<List<Coord>> clipped = LineClipper.clip(bounds, points);
 			if (clipped != null) {
 				log.info("clipping", segment);
-				if (clipped.size() > 0) {
-					// the LineClipper sometimes returns un-joined clips
-					// need to rejoin them here
-					log.info(clipped.size(),"clippings. Try to join them.");
-					List<Way> clippedWays = new ArrayList<Way>(clipped.size());
-					for (List<Coord> clippedPoints : clipped) {
-						clippedWays.add(new Way(FakeIdGenerator.makeFakeId(), clippedPoints));
-					}
-					clippedWays = joinWays(clippedWays);
-					if (clippedWays.size() != clipped.size()) {
-						clipped = new ArrayList<List<Coord>>(clippedWays.size());
-						for (Way w : clippedWays) {
-							clipped.add(w.getPoints());
-						}
-					}
-					log.info(clipped.size(),"joined clippings.");
-				}
 				toBeRemoved.add(segment);
 				for (List<Coord> pts : clipped) {
 					long id = FakeIdGenerator.makeFakeId();
@@ -479,7 +462,7 @@ public class SeaGenerator extends OsmReadingHooksAdaptor {
 			}
 		}
 
-		log.info("clipping: adding ", toBeAdded.size(), ", removing ", toBeRemoved.size());
+		log.info("clipping: adding", toBeAdded.size(), ", removing", toBeRemoved.size());
 		shoreline.removeAll(toBeRemoved);
 		shoreline.addAll(toBeAdded);
 	}
