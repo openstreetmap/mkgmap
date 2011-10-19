18b747edcc6def88cac6b01335582afb6edb357b
wanmil <wanmil@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2011-10-19 19:17:17 +0000

    Boundary precompiler uses a finer detection if a boundary is usable
    The detection if a boundary is usable now checks also if a name tag (a tagname containing the string 'name') is set which is mandatory to be used by the LocationHook. Otherwise the boundaries blow up the precompiled bounds but must be sorted out by the LocationHook. When doing this in the precompilation step it saves time while generating the real map.
    
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@2051 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/mkgmap/reader/osm/boundary/BoundaryElementSaver.java
+++ src/uk/me/parabola/mkgmap/reader/osm/boundary/BoundaryElementSaver.java
@@ -14,6 +14,7 @@ package uk.me.parabola.mkgmap.reader.osm.boundary;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Map.Entry;
 
 import uk.me.parabola.log.Logger;
 import uk.me.parabola.mkgmap.reader.osm.Element;
@@ -31,8 +32,7 @@ import uk.me.parabola.util.EnhancedProperties;
  * @author WanMil
  */
 public class BoundaryElementSaver extends ElementSaver {
-	private static final Logger log = Logger
-	.getLogger(BoundaryElementSaver.class);
+	private static final Logger log = Logger.getLogger(BoundaryElementSaver.class);
 
 	private final BoundarySaver saver;
 	
@@ -41,21 +41,86 @@ public class BoundaryElementSaver extends ElementSaver {
 		this.saver = saver;
 	}
 
+	/**
+	 * Checks if the given element is an administrative boundary or a
+	 * postal code area.
+	 * @param element an element
+	 * @return <code>true</code> administrative boundary or postal code; 
+	 * <code>false</code> element cannot be used for precompiled bounds 
+	 */
 	public static boolean isBoundary(Element element) {
 		if (element instanceof Relation) {
 			String type = element.getTag("type");
-			if ("boundary".equals(type)) {
-				return true;
+			
+			if ("boundary".equals(type) || "multipolygon".equals(type)) {
+				String boundaryVal = element.getTag("boundary");
+				if ("administrative".equals(boundaryVal)) {
+					// for boundary=administrative the admin_level must be set
+					if (element.getTag("admin_level") == null) {
+						return false;
+					}
+					// and a name must be set (check only for a tag containing name
+					for (Entry<String,String> tag : element.getEntryIteratable()) {
+						if (tag.getKey().contains("name")) {
+							return true;
+						}
+					}
+					// does not contain a name tag => do not use it
+					return false;					
+				} else if ("postal_code".equals(boundaryVal)) {
+					// perform a positive check
+					
+					// is postal_code set?
+					if (element.getTag("postal_code") != null) {
+						return true;
+					}
+					// and a name must be set (check only for a tag containing name
+					for (Entry<String,String> tag : element.getEntryIteratable()) {
+						if (tag.getKey().contains("name")) {
+							return true;
+						}
+					}
+					// does not contain a name tag => do not use it
+					return false;						
+				} else if (element.getTag("postal_code") != null){
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
 			}
-			String boundaryVal = element.getTag("boundary");
-			if ("multipolygon".equals(type)
-					&& ("administrative".equals(boundaryVal) || "postal_code".equals(boundaryVal))) {
-				return true;
-			}
-			return false;
 		} else if (element instanceof Way) {
+			Way w = (Way) element;
+			// a single way must be closed
+			if (w.isClosed() == false) {
+				return false;
+			}
+			// the boundary tag must be "administrative" or "postal_code"
 			String boundaryVal = element.getTag("boundary");
-			return ("administrative".equals(boundaryVal) || "postal_code".equals(boundaryVal));
+			if ("administrative".equals(boundaryVal)) {
+				// for boundary=administrative the admin_level must be set
+				if (element.getTag("admin_level") == null) {
+					return false;
+				}
+				
+				// and a name must be set (check only for a tag containing name)
+				for (Entry<String,String> tag : element.getEntryIteratable()) {
+					if (tag.getKey().contains("name")) {
+						return true;
+					}
+				}
+				// does not contain a name tag => do not use it
+				return false;
+			} else if ( "postal_code".equals(boundaryVal)) {
+				// the name tag must be set for it
+				return element.getTag("name") != null;
+			} else if (element.getTag("postal_code") != null) {
+				// postal_code as tag
+				return true;
+			} else {
+				return false;
+			}
 		} else {
 			return false;
 		}
@@ -69,7 +134,7 @@ public class BoundaryElementSaver extends ElementSaver {
 			if (b != null)
 				saver.addBoundary(b);
 		} else {
-			log.error("Relation is not processed due to missing tags: "+rel.getId()+" "+rel.toTagString());
+			log.warn("Relation is not processed due to missing tags:", rel.getId(), rel.toTagString());
 		}
 	}
 	
