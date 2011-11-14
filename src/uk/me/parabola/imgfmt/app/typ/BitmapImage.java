9793b968feb7adfdb68758cd3b54428c2279385b
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2011-11-14 08:34:27 +0000

    Fix bug with colormode=0x20 and pixmaps with small numbers of bits.
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/branches/typ@2084 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/imgfmt/app/typ/BitmapImage.java
+++ src/uk/me/parabola/imgfmt/app/typ/BitmapImage.java
@@ -25,31 +25,28 @@ public class BitmapImage implements Writeable, Comparator<BitmapImage> {
 	}
 
 	public void write(ImgFileWriter writer) {
-		BitWriter bitWriter = new BitWriter();
 
 		final int bitSize = colourInfo.getBitsPerPixel();
 		int cpp = colourInfo.getCharsPerPixel();
 
 		int width = colourInfo.getWidth();
 		int height = colourInfo.getHeight();
-		int adjwidth = (width&1)==1? width+1: width;
 
 		int i = 0;
 		for (int h = 0; h < height; h++) {
-			for (int w = 0; w < adjwidth; w++) {
-				if (w >= width) {
-					bitWriter.putn(0, bitSize);
-				} else {
-					String idx = image.substring(i, i + cpp);
-					i += cpp;
+			// Each row is padded to a byte boundary, creating a new bit writer for every
+			// row ensures that happens.
+			BitWriter bitWriter = new BitWriter();
 
-					int val = colourInfo.getIndex(idx);
-					bitWriter.putn(val, bitSize);
-				}
+			for (int w = 0; w < width; w++) {
+				String idx = image.substring(i, i + cpp);
+				i += cpp;
+
+				int val = colourInfo.getIndex(idx);
+				bitWriter.putn(val, bitSize);
 			}
+			writer.put(bitWriter.getBytes(), 0, bitWriter.getLength());
 		}
-
-		writer.put(bitWriter.getBytes(), 0, bitWriter.getLength());
 	}
 
 	public int compare(BitmapImage a, BitmapImage b) {
