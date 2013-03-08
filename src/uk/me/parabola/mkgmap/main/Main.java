e0fb7fbf6d0797f0b385e344121cbbdb44f1abb1
wanmil <wanmil@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2013-03-08 18:35:41 +0000

    Use System.out instead of System.err for the Java < 7 warning so that the tests don't fail
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@2521 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/mkgmap/main/Main.java
+++ src/uk/me/parabola/mkgmap/main/Main.java
@@ -126,12 +126,12 @@ public class Main implements ArgumentProcessor {
 			if (versionParts.length >= 2) {
 				int major = Integer.valueOf(versionParts[1]);
 				if (major < 7) {
-					System.err.println("===========================================================");
-					System.err.println("You are using an old Java runtime environment "+version);
-					System.err.println("mkgmap support for JRE 1.6.x will be discontinued");
-					System.err.println("after June 2013.");
-					System.err.println("Please update the JRE to the latest release.");
-					System.err.println("===========================================================");
+					System.out.println("===========================================================");
+					System.out.println("You are using an old Java runtime environment "+version);
+					System.out.println("mkgmap support for JRE 1.6.x will be discontinued");
+					System.out.println("after June 2013.");
+					System.out.println("Please update the JRE to the latest release.");
+					System.out.println("===========================================================");
 				}
 			}
 		}
