d39c4aebb4cf95aa4c086e5d0151ef3745b48d6b
gerd <gerd@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2013-04-04 09:18:46 +0000

    Implement basic configuration file reader for overview map. Use option --x-overview-cfg=cfgfile to specify a file that contains lines like this: # configuration of overview map # format: object-type:type # object-type is one of point,line, or polygon # type is an integer or * (asterisk) to include all types point:* line:* polygon:0x3b polygon:0x3e
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/branches/overview2@2549 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/mkgmap/combiners/OverviewBuilder.java
+++ src/uk/me/parabola/mkgmap/combiners/OverviewBuilder.java
@@ -13,14 +13,21 @@
 package uk.me.parabola.mkgmap.combiners;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import uk.me.parabola.imgfmt.ExitException;
 import uk.me.parabola.imgfmt.FileExistsException;
 import uk.me.parabola.imgfmt.FileNotWritableException;
 import uk.me.parabola.imgfmt.FileSystemParam;
 import uk.me.parabola.imgfmt.MapFailedException;
+import uk.me.parabola.imgfmt.Utils;
 import uk.me.parabola.imgfmt.app.Area;
 import uk.me.parabola.imgfmt.app.Coord;
 import uk.me.parabola.imgfmt.app.map.Map;
@@ -57,6 +64,8 @@ public class OverviewBuilder implements Combiner {
 	private boolean addPoints = false;
 	private boolean addLines = false;
 	private boolean addPolygons = false;
+	HashMap<String, HashSet<Integer>> typeFilterMap = new HashMap<String, HashSet<Integer>>();
+	private String overviewConfig;
 
 	public OverviewBuilder(OverviewMap overviewSource) {
 		this.overviewSource = overviewSource;
@@ -71,7 +80,56 @@ public class OverviewBuilder implements Combiner {
 		addPoints = args.getProperties().containsKey("overview-add-points");
 		addLines = args.getProperties().containsKey("overview-add-lines");
 		addPolygons = args.getProperties().containsKey("overview-add-polygons");
-		
+		overviewConfig = args.get("overview-cfg", null);
+		if (overviewConfig != null)
+			readConfig();
+	}
+
+	private void readConfig() {
+		String[] objects = {"point","line","polygon"}; 
+		try {
+			LineNumberReader cfgReader = new LineNumberReader(new InputStreamReader(Utils.openFile(overviewConfig)));
+					
+			Pattern csvSplitter = Pattern.compile(Pattern.quote(":"));
+			String cfgLine = null;
+
+			try {
+				while ((cfgLine = cfgReader.readLine()) != null) {
+					if (cfgLine.startsWith("#")) {
+						// comment
+						continue; 
+					}
+					String[] items = csvSplitter.split(cfgLine); 
+					//System.out.println(items.length + " " + cfgLine);
+					if (items.length < 2){
+						log.warn("Invalid format in ",
+								cfgLine);
+						continue; 					
+					}
+					for (String object :objects){
+						if (object.equals(items[0])){
+							HashSet<Integer> set = typeFilterMap.get(object);
+							if (set == null){
+								set = new HashSet<Integer>();
+								typeFilterMap.put(object, set);
+							}
+							if ("*".equals(items[1])){
+								set.add(Integer.MAX_VALUE);
+							}
+							else {
+								Integer type = Integer.decode(items[1]);
+								set.add(type);
+							}
+						}
+					}
+				}
+				cfgReader.close();
+			} catch (IOException e) {
+				log.error("Can't read file " + overviewConfig);
+			}
+		} catch (FileNotFoundException e1) {
+			log.error("Can't open file " + overviewConfig);
+		}
 	}
 
 	public void onMapEnd(FileInfo finfo) {
@@ -123,11 +181,11 @@ public class OverviewBuilder implements Combiner {
 
 		levels = mapReader.getLevels();
 
-		if (addPoints)
+		if (addPoints || typeFilterMap.containsKey("point"))
 			readPoints(mapReader);
-		if (addLines)
+		if (addLines || typeFilterMap.containsKey("line"))
 			readLines(mapReader);
-		if (addPolygons)
+		if (addPolygons || typeFilterMap.containsKey("polygon"))
 			readShapes(mapReader);
 	}
 
@@ -135,17 +193,25 @@ public class OverviewBuilder implements Combiner {
 	 * Read the points from the .img file and add them to the overview map.
 	 * We read from the least detailed level (apart from the empty one).
 	 *
-	 * TODO: further filter what is seen
-	 *
 	 * @param mapReader Map reader on the detailed .img file.
 	 */
 	private void readPoints(MapReader mapReader) {
+		HashSet<Integer> filterSet = typeFilterMap.get("point");
+		boolean addAll;
+		if (filterSet!= null) 
+			addAll = (filterSet.contains(Integer.MAX_VALUE));
+		else 
+			addAll = true;
+
 		int min = levels[1].getLevel();
 		List<Point> pointList = mapReader.pointsForLevel(min, MapReader.WITH_EXT_TYPE_DATA);
 		for (Point point: pointList) {
 			log.debug("got point", point);
+			if (!addAll){
+				if (filterSet.contains(point.getType()) == false)
+					continue;
+			}
 			MapPoint mp = new MapPoint();
-
 			mp.setType(point.getType());
 			mp.setName(point.getLabel().getText());
 			mp.setMaxResolution(24); // TODO
@@ -159,15 +225,25 @@ public class OverviewBuilder implements Combiner {
 	 * Read the lines from the .img file and add them to the overview map.
 	 * We read from the least detailed level (apart from the empty one).
 	 *
-	 * TODO: further filter what is seen
-	 *
 	 * @param mapReader Map reader on the detailed .img file.
 	 */
 	private void readLines(MapReader mapReader) {
+		HashSet<Integer> filterSet = typeFilterMap.get("line");
+		boolean addAll;
+		if (filterSet!= null) 
+			addAll = (filterSet.contains(Integer.MAX_VALUE));
+		else 
+			addAll = true;
+
 		int min = levels[1].getLevel();
 		List<Polyline> lineList = mapReader.linesForLevel(min);
 		for (Polyline line : lineList) {
 			log.debug("got line", line);
+			if (!addAll){
+				if (filterSet.contains(line.getType()) == false)
+					continue;
+			}
+			
 			MapLine ml = new MapLine();
 
 			List<Coord> list = line.getPoints();
@@ -186,14 +262,31 @@ public class OverviewBuilder implements Combiner {
 		}
 	}
 
+	/**
+	 * Read the polygons from the .img file and add them to the overview map.
+	 * We read from the least detailed level (apart from the empty one).
+	 *
+	 * @param mapReader Map reader on the detailed .img file.
+	 */
 	private void readShapes(MapReader mapReader) {
+		HashSet<Integer> filterSet = typeFilterMap.get("polygon");
+		boolean addAll;
+		if (filterSet!= null) 
+			addAll = (filterSet.contains(Integer.MAX_VALUE));
+		else 
+			addAll = true;
+		
 		int min = levels[1].getLevel();
 		List<Polygon> list = mapReader.shapesForLevel(min);
 		for (Polygon shape : list) {
+			if (!addAll){
+				if (filterSet.contains(shape.getType()) == false)
+					continue;
+			}
 			MapShape ms = new MapShape();
 
 			List<Coord> points = shape.getPoints();
-			if (points.size() < 2)
+			if (points.size() < 3)
 				continue;
 
 			ms.setType(shape.getType());
