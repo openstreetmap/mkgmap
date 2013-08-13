15cbf4101bf97c19dd2db627b139cad07c77f02a
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2013-08-13 13:33:46 +0000

    PartFilter extract 1) from end, 2) initial and final ranges.
    Use negative numbers to take a part from the tail of the value list.  Use
    the "<" or ">" operator (in addition to ":") in order to return a
    concatenation of all the parts before or after a certain part. The
    default behavior is not changed and this come at the expense of just 2
    logical "IF" more when no operators are specified.
    
    Example, if the value is "Aa#Bb#Cc#Dd#Ee"
    part:#:1  returns Aa
    part:#:-1 returns Ee
    part:#:-2 returns Dd
    part:#>1  returns Bb#Cc#Dd#Ee#
    part:#<5  returns Aa#Bb#Cc#Dd#
    part:#<-1 returns Aa#Bb#Cc#Dd#
    
    - Enrico Liboni
    
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@2668 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/mkgmap/osmstyle/actions/PartFilter.java
+++ src/uk/me/parabola/mkgmap/osmstyle/actions/PartFilter.java
@@ -17,25 +17,60 @@ import uk.me.parabola.imgfmt.ExitException;
 import uk.me.parabola.mkgmap.reader.osm.Element;
 
 /**
- * Get a part of a value.
+ * Split a value in parts and returns one or more part(s) of a value.
  * value is split at a separator that defaults to semicolon ';'
  * by default the first part is returned
- * if the optional second parameter 'partnumber' is bigger 
- * than the number of parts in the value then null is returned
+ *
+ * if the optional second parameter 'partnumber' is out of
+ * range then null is returned
+ *
+ * if the optional second parameter is negative
+ * the part returned is counted from the end of the split
+ *
+ * if the the split operator is a > or < and the
+ * the correspondent number of parts are returned
+ *
+ * Example: if the value is "Aa#Bb#Cc#Dd#Ee"
+ * part:#:1  returns Aa
+ * part:#:-1 returns Ee
+ * part:#:2  returns Bb
+ * part:#:-2 returns Dd
+ * part:#>1  returns Bb#Cc#Dd#Ee#
+ * part:#<5  returns Aa#Bb#Cc#Dd#
+ * part:#<-1 returns Aa#Bb#Cc#Dd#
  *
  * @author Franco Bez
+ * @author Enrico Liboni
  */
 public class PartFilter extends ValueFilter {
 	private String separator;
 	private int partnumber;
+	private boolean isLt=false; // less than  <
+	private boolean isGt=false; // great than >
 
 	public PartFilter(String arg) {
-		String[] temp = arg.split(":");
+		String[] temp;
+
+		// detect which operator is used
+		if (arg.contains(":")) {
+			temp = arg.split(":");
+		} else if (arg.contains(">")) {
+			temp = arg.split(">");
+			isGt = true;
+		} else if (arg.contains("<")) {
+			temp = arg.split("<");
+			isLt = true;
+		} else { // no operators default to arg
+			temp =  new String[] { arg };
+		}
+
 		partnumber = 1;
 		try {
+			// set the part number (default is 1)
 			if( temp.length > 1 ) {
 				partnumber = Integer.parseInt(temp[1]);
 			}
+			// set the separator (default to ;)
 			if(temp[0].length() > 0 ){
 				separator = temp[0];
 			}
@@ -48,11 +83,39 @@ public class PartFilter extends ValueFilter {
 	}
 
 	public String doFilter(String value, Element el) {
-		if (value == null) return null;
+		if (value == null || partnumber == 0) return null;
 		// split uses a regex we need to replace special characters
 		String[] temp = value.split(Pattern.quote(separator));
-		if (temp.length >= partnumber)
-			return temp[partnumber-1].trim();
-		return null;
+
+		// check if partnumber is in range, if not return null
+		if (temp.length < Math.abs(partnumber) ) return null;
+
+		// get the index of the partnumber
+		// if the partnumber is negative the part is counted from the end of the split
+		int idx=(partnumber > 0)?(partnumber-1):(temp.length+partnumber);
+
+		// default operator ":": return the part
+		if ( !isLt && !isGt ) {
+			return temp[idx].trim();
+		} else {
+			StringBuffer returnValue= new StringBuffer();
+
+			// operator "<": collate all the parts before the partnumber
+			if ( isLt ) {
+				for (int i=0;i<idx;i++) {
+					returnValue.append(temp[i]).append(separator);
+				}
+			}
+
+			// operator ">": collate all the parts after the partnumber
+			if ( isGt ) {
+				for (int i=idx+1;i<temp.length;i++) {
+					returnValue.append(temp[i]).append(separator);
+				}
+			}
+
+			// return the result
+			return returnValue.toString();
+		}
 	}
 }
