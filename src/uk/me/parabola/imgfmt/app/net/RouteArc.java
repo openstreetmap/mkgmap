edab70af25b209fd262a189e62050ea360c9c0be
gerd <gerd@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2013-10-20 13:56:45 +0000

    remove out-aged error message regarding arc length
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/branches/reduce_limits@2777 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/imgfmt/app/net/RouteArc.java
+++ src/uk/me/parabola/imgfmt/app/net/RouteArc.java
@@ -262,12 +262,8 @@ public class RouteArc {
 		// update haveCurve
 		haveCurve = (curveEnabled && finalHeading != initialHeading);
 
-		if (length >= (1 << 14)) {
-			log.error("warning: Way " + roadDef.getName() + " (id " + roadDef.getId() + ") contains an arc whose length (" + length + " units) is too big to be encoded so the way will not be routable");
-			//length = (1 << 14) - 1;
-		}
 		if (length >= (1 << 22)) {
-			log.error("Way " + roadDef.getName() + " (id " + roadDef.getId() + ") contains an arc whose length (" + length + " units) is too big to be encoded so the way will not be routable");
+			log.error("Way " + roadDef.getName() + " (id " + roadDef.getId() + ") contains an arc whose length (" + length + " units) is too big to be encoded so the way might not be routable");
 			length = (1 << 22) - 1;
 		}
 
