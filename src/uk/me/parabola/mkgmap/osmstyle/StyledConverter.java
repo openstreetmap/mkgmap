2bd6b2b394984b3a10eb181900b1ec9854ade0a1
gerd <gerd@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2013-12-01 21:06:15 +0000

    correction: detect zero arc length at end of road
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/branches/high-prec-coord@2858 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/mkgmap/osmstyle/StyledConverter.java
+++ src/uk/me/parabola/mkgmap/osmstyle/StyledConverter.java
@@ -1987,7 +1987,7 @@ public class StyledConverter implements OsmConverter {
 				// scan through the way's points looking for points which are
 				// close together
 				Coord prev = null;
-				for (int i = 0; i < points.size() - 1; ++i) {
+				for (int i = 0; i < points.size(); ++i) {
 					Coord p = points.get(i);
 					if (i > 0) {
 						if (p == prev) {
