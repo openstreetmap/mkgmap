ebdfb47a278b1d8e5a74c32edf151e403c26d93f
gerd <gerd@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2013-12-18 07:04:52 +0000

    now really make sure that returned shapes begin and end with identical points. Added unit test to verify special cases with equal points
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@2888 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- test/uk/me/parabola/util/Java2DConverterTest.java
+++ test/uk/me/parabola/util/Java2DConverterTest.java
@@ -13,7 +13,9 @@
 package uk.me.parabola.util;
 
 import java.awt.geom.Area;
+import java.awt.geom.Path2D;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import uk.me.parabola.imgfmt.app.Coord;
@@ -34,7 +36,7 @@ public class Java2DConverterTest {
 		polygon.add(new Coord(120,89));
 		polygon.add(new Coord(20,44));
 		polygon.add(new Coord(50,43));
-		polygon.add(new Coord(0,0));
+		polygon.add(new Coord(0,0)); // Note that shape is not closed with identical points
 		
 		Area a = Java2DConverter.createArea(polygon);
 		List<List<Coord>> convPolygon = Java2DConverter.areaToShapes(a);
@@ -43,6 +45,56 @@ public class Java2DConverterTest {
 		assertEquals(convPolygon.size(), 1);
 		assertEquals(polygon, convPolygon.get(0));
 		assertEquals(polygon, singularPolygon);
+	}
+	
+	@Test
+	public void testPolygonConversionWithEqualPoints() throws Exception {
+		Path2D path1 = new Path2D.Double();
+		Path2D path2 = new Path2D.Double();
+		Path2D path3 = new Path2D.Double();
+		path1.moveTo(0,0);
+		path1.lineTo(100,-10);
+		path1.lineTo(50,-43);
+		path1.closePath();
+
+		path2.moveTo(0,0);
+		path2.lineTo(100,-10);
+		path2.lineTo(50,-43);
+		path2.lineTo(-0.0001,0); // point that is equal to closing points (in map units)  
+		path2.closePath();
+		
+		path3.moveTo(0,0);
+		path3.lineTo(100,-10);
+		path3.lineTo(50,-43);
+		path3.lineTo(-0.5001,0); // point that is not equal to closing points (in map units)  
+		path3.closePath();
+		
+		Area a1 = new Area(path1);
+		Area a2 = new Area(path2);
+		Area a3 = new Area(path3);
+		
+		List<List<Coord>> convPolygon1 = Java2DConverter.areaToShapes(a1);
+		List<List<Coord>> convPolygon2 = Java2DConverter.areaToShapes(a2);
+		List<List<Coord>> convPolygon3 = Java2DConverter.areaToShapes(a3);
+		List<Coord> singularPolygon1 = Java2DConverter.singularAreaToPoints(a1);
+		List<Coord> singularPolygon2 = Java2DConverter.singularAreaToPoints(a2);
+		List<Coord> singularPolygon3 = Java2DConverter.singularAreaToPoints(a3);
+		
+		assertTrue(a1.equals(a2) == false);
+		assertTrue(a1.equals(a3) == false);
+		assertTrue(a2.equals(a3) == false);
+		assertEquals(1, convPolygon1.size());
+		assertEquals(1, convPolygon2.size());
+		assertEquals(1, convPolygon3.size());
+		assertTrue(Arrays.deepEquals(convPolygon1.toArray(), convPolygon2.toArray()) == true); 
+		assertTrue(Arrays.deepEquals(convPolygon1.toArray(), convPolygon3.toArray()) == false); 
+		assertEquals(4, convPolygon1.get(0).size());
+		assertEquals(4, convPolygon2.get(0).size());
+		assertEquals(5, convPolygon3.get(0).size());
+		assertEquals(4,singularPolygon1.size());
+		assertEquals(4,singularPolygon2.size());
+		assertEquals(5,singularPolygon3.size());
+		
 	}
 	
 	
