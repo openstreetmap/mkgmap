7e0bfa853426407a6256a2c5ec75fed56f4ba158
wanmil <wanmil@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2014-01-16 20:18:49 +0000

    Avoid precalculation of area size for sea polygons created by the SeaGenerator.
    For all sea polygons the area size was added as tag which increases the file size of the sea tiles but does not really help. The size only calculated the size of the sea in one sea tile and it's a bit random where the borders of a sea tile are. So the area size is not usefull.
    
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@2971 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- src/uk/me/parabola/mkgmap/reader/osm/SeaPolygonRelation.java
+++ src/uk/me/parabola/mkgmap/reader/osm/SeaPolygonRelation.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2014.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 or
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
 package uk.me.parabola.mkgmap.reader.osm;
 
 import java.text.DecimalFormat;
@@ -43,7 +56,10 @@ public class SeaPolygonRelation extends MultiPolygonRelation {
 		addTag("type", "mkgmap:seapolygon");
 	}
 
-	@Override
+	protected boolean isAreaSizeCalculated() {
+		return false;
+	}
+	
 	protected void postProcessing() {
 		if (isFloodBlocker()) {
 			removeFloodedAreas();
