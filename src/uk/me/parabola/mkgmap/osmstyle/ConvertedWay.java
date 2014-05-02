openstreetmap/mkgmap

commit f30032015ecc9be82eef376d3e1fe41704c7f953

- print error message when style assigns non-routable type in combination with road_class or road_speed. Consider only ways with routable types and road_class/road_speed as roads.
- change RoadMerger.merge() to return the result



git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@3227 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/mkgmap/osmstyle/ConvertedWay.java
@@ -33,25 +33,29 @@ public class ConvertedWay {
 	private static final Logger log = Logger.getLogger(ConvertedWay.class);
 	private final int index;
 	private final Way way;				// with tags after Style processing
-	private final GType gt;								
+	private final GType gt;
 
 	private byte roadClass;			// 0-4
 	private byte roadSpeed;			// 0-7
 	private byte mkgmapAccess; 		// bit mask, see ACCESS_TAGS 
 	private final byte routeFlags;	// bit mask, see ROUTING_TAGS
+	private boolean isRoad;
+	
 	
 
 	public ConvertedWay(int index, Way way, GType type) {
 		this.index = index;
 		this.way = way;
 		this.gt = type;
-		if (type.isRoad()) {
+		this.isRoad = type.isRoad();
+		if (type.isRoad() & GType.isRoutableLineType(type.getType())) {
 			this.roadClass = (byte) gt.getRoadClass();
 			this.roadSpeed = (byte) gt.getRoadSpeed();
 			recalcRoadClass(way);
 			recalcRoadSpeed(way);
 			mkgmapAccess = evalAccessTags(way);
 			routeFlags = evalRouteTags(way);
+			isRoad = true;
 		} else {
 			roadClass = 0;
 			roadSpeed = 0;
@@ -251,5 +255,9 @@ public class ConvertedWay {
 
 	public boolean isThroughroute(){
 		return (routeFlags & R_THROUGHROUTE) != 0; 
+	}
+	
+	public boolean isRoad(){
+		return isRoad;
 	}
 }

src/uk/me/parabola/mkgmap/osmstyle/StyledConverter.java
@@ -192,6 +192,7 @@ public class StyledConverter implements OsmConverter {
 		}
 	}
 	
+	
 	/**
 	 * This takes the way and works out what kind of map feature it is and makes
 	 * the relevant call to the mapper callback.
@@ -201,12 +202,12 @@ public class StyledConverter implements OsmConverter {
 	 *
 	 * @param way The OSM way.
 	 */
+	private long lastRoadId = 0; 
 	public void convertWay(final Way way) {
 		if (way.getPoints().size() < 2 || way.getTagCount() == 0){
 			// no tags or no points => nothing to convert
 			removeRestrictionsWithWay(Level.WARNING, way, "is ignored");
 		}
-
 		preConvertRules(way);
 
 		housenumberGenerator.addWay(way);
@@ -239,47 +240,55 @@ public class StyledConverter implements OsmConverter {
 			wayTypeResult.setWay(cycleWay);
 			rules.resolveType(cycleWay, wayTypeResult);
 		}
-		if (roads.isEmpty() || roads.get(roads.size()-1).getWay().getId() != way.getId()){
+		if (lastRoadId != way.getId()){
+			// this way was not added to the roads list
 			removeRestrictionsWithWay(Level.WARNING, way, "is not routable");
 		}
 	}
 
-	private int roadIndex = 0;
 	private int lineIndex = 0;
+	private boolean wrongTypeMsgPrinted = false;
 	private void addConvertedWay(Way way, GType foundType) {
-		if (foundType.getFeatureKind() == FeatureKind.POLYLINE) {
-			
-			String oneWay = way.getTag("oneway");
-			boolean wasReversed = false;
-			if("-1".equals(oneWay) || "reverse".equals(oneWay)) {
-				// it's a oneway street in the reverse direction
-				// so reverse the order of the nodes and change
-				// the oneway tag to "yes"
-				way.reverse();
-				wasReversed = true;
-				way.addTag("oneway", "yes");
-			}
-
-			if (way.tagIsLikeYes("oneway")) {
-				way.addTag("oneway", "yes");
-				if (foundType.isRoad() && checkFixmeCoords(way) )
-					way.addTag("mkgmap:dead-end-check", "false");
-			} else 
-				way.deleteTag("oneway");
-			
-			if(foundType.isRoad() && !MapObject.hasExtendedType(foundType.getType())){
-				ConvertedWay cw = new ConvertedWay(roadIndex++, way, foundType);
-				if (wasReversed && cw.isRoundabout())
-					log.warn("Roundabout", way.getId(), "has reverse oneway tag (" + way.getPoints().get(0).toOSMURL() + ")");
-		    	roads.add(cw);
-			}
-		    else { 
-				ConvertedWay cw = new ConvertedWay(lineIndex++, way, foundType);
-		    	lines.add(cw);
-		    }
-		}
-		else
+		if (foundType.getFeatureKind() == FeatureKind.POLYGON){ 
 			addShape(way, foundType);
+			return;
+		}
+		
+		String oneWay = way.getTag("oneway");
+		boolean wasReversed = false;
+		if("-1".equals(oneWay) || "reverse".equals(oneWay)) {
+			// it's a oneway street in the reverse direction
+			// so reverse the order of the nodes and change
+			// the oneway tag to "yes"
+			way.reverse();
+			wasReversed = true;
+			way.addTag("oneway", "yes");
+		}
+
+		if (way.tagIsLikeYes("oneway")) {
+			way.addTag("oneway", "yes");
+			if (foundType.isRoad() && checkFixmeCoords(way) )
+				way.addTag("mkgmap:dead-end-check", "false");
+		} else 
+			way.deleteTag("oneway");
+		ConvertedWay cw = new ConvertedWay(lineIndex++, way, foundType);
+		if (cw.isRoad()){
+			roads.add(cw);
+			if (wasReversed && cw.isRoundabout())
+				log.warn("Roundabout", way.getId(), "has reverse oneway tag (" + way.getPoints().get(0).toOSMURL() + ")");
+			lastRoadId = way.getId();
+		}
+		else 
+			lines.add(cw);
+
+		// old code allowed all non-extended types for roads. Now print error message in this case.
+		if(foundType.isRoad() && !cw.isRoad()){
+			if (wrongTypeMsgPrinted == false){
+				log.error("Non-routable type in combination with road_class/road_speed used for routable map.",
+						"Try --check-styles to check the style.");
+
+			}
+		}
 	}
 
 	/** One type result for nodes to avoid recreating one for each node. */ 
@@ -412,7 +421,7 @@ public class StyledConverter implements OsmConverter {
 	}
 
 	/**
-	 * Merges roads with identical attributes (gtype, OSM tags) to reduce the size of the 
+	 * Merges roads with identical attributes (GType, OSM tags) to reduce the size of the 
 	 * road network.
 	 */
 	private void mergeRoads() {
@@ -421,12 +430,8 @@ public class StyledConverter implements OsmConverter {
 			return;
 		}
 		
-		// instantiate the RoadMerger - the roads and roadTypes lists are copied
-		RoadMerger merger = new RoadMerger(roads);
-		// clear the lists
-		roads.clear();
-		// merge the roads and copy the results to the roads and roadTypes list
-		merger.merge(roads, restrictions, throughRouteRelations);
+		RoadMerger merger = new RoadMerger();
+		roads = merger.merge(roads, restrictions, throughRouteRelations);
 	}
 	
 	public void end() {
@@ -462,7 +467,7 @@ public class StyledConverter implements OsmConverter {
 		modifiedRoads = null;
 
 		mergeRoads();
-
+		
 		resetHighwayCounts();
 		setHighwayCounts();
 		
@@ -473,7 +478,7 @@ public class StyledConverter implements OsmConverter {
 		lines = null;
 		if (roadLog.isInfoEnabled()) {
 			roadLog.info("Flags: oneway,no-emergency, no-delivery, no-throughroute, no-truck, no-bike, no-foot, carpool, no-taxi, no-bus, no-car");
-			roadLog.info(String.format("%19s %4s %11s %s", "Road-OSM-Id","Type","Flags", "Labels"));
+			roadLog.info(String.format("%19s %4s %11s %6s %s", "Road-OSM-Id","Type","Flags", "Points", "Labels"));
 		}
 		// add the roads after the other lines
 		for (ConvertedWay cw : roads){
@@ -1499,7 +1504,7 @@ public class StyledConverter implements OsmConverter {
 				cmpAccess |= 1<<10;
 			}
 			String access = String.format("%11s",Integer.toBinaryString(cmpAccess)).replace(' ', '0');
-			roadLog.info(String.format("%19d 0x%-2x %11s %s", way.getId(), road.getType(), access, Arrays.toString(road.getLabels())));
+			roadLog.info(String.format("%19d 0x%-2x %11s %6d %s", way.getId(), road.getType(), access, road.getPoints().size(),Arrays.toString(road.getLabels())));
 		}
 		
 		// add the road to the housenumber generator

src/uk/me/parabola/mkgmap/osmstyle/RoadMerger.java
@@ -49,9 +49,6 @@ public class RoadMerger {
 	/** maps which coord of a way(id) are restricted - they should not be merged */
 	private final MultiIdentityHashMap<Coord, Long> restrictions = new MultiIdentityHashMap<>();
 
-	/** Contains a list of all roads (GType + Way) */
-	private final List<ConvertedWay> roads;
-
 	/** maps the start point of a road to its road definition */
 	private final MultiIdentityHashMap<Coord, ConvertedWay> startPoints = new MultiIdentityHashMap<>();
 	/** maps the end point of a road to its road definition */
@@ -72,7 +69,6 @@ public class RoadMerger {
 			add("mkgmap:country");
 			add("mkgmap:is_in");
 			add("mkgmap:skipSizeFilter");
-			add("junction"); // should be removed, only junction=roundabout matters
 			add("mkgmap:synthesised");
 			add("mkgmap:highest-resolution-only");
 			add("mkgmap:flare-check");
@@ -92,16 +88,6 @@ public class RoadMerger {
 		return s1.equals(s2);
 	}
 
-
-	public RoadMerger(List<ConvertedWay> convertedWays) {
-		this.roads = new ArrayList<>(convertedWays.size());
-
-		for (int i = 0; i < convertedWays.size(); i++) {
-			ConvertedWay cw = convertedWays.get(i);
-			if (cw.isValid())
-				roads.add(cw);
-		}
-	}
 
 	/**
 	 * We must not merge roads at via points of restriction relations
@@ -224,14 +210,19 @@ public class RoadMerger {
 	 * @param resultingWays list for the merged (and not mergeable) ways
 	 * @param resultingGTypes list for the merged (and not mergeable) GTypes
 	 */
-	public void merge(List<ConvertedWay> resultingWays,
+	public List<ConvertedWay>  merge(List<ConvertedWay> convertedWays,
 			List<RestrictionRelation> restrictions,
 			List<Relation> throughRouteRelations) {
+		List<ConvertedWay> result = new ArrayList<>();
+		List<ConvertedWay> roadsToMerge = new ArrayList<>(convertedWays.size());
+		for (int i = 0; i < convertedWays.size(); i++) {
+			ConvertedWay cw = convertedWays.get(i);
+			if (cw.isValid() && cw.isRoad())
+				roadsToMerge.add(cw);
+		}
 
-		int noRoadsBeforeMerge = this.roads.size();
+		int noRoadsBeforeMerge = roadsToMerge.size();
 		int noMerges = 0;
-		List<ConvertedWay> roadsToMerge = new ArrayList<>(this.roads);
-		this.roads.clear();
 		
 		List<Coord> mergePoints = new ArrayList<>();
 
@@ -244,7 +235,7 @@ public class RoadMerger {
 
 			if (start == end) {
 				// do not merge closed roads
-				roads.add(road);
+				result.add(road);
 				continue;
 			}
 
@@ -337,26 +328,24 @@ public class RoadMerger {
 
 		// copy all merged roads to the roads list
 		for (List<ConvertedWay> mergedRoads : endPoints.values()) {
-			this.roads.addAll(mergedRoads);
+			result.addAll(mergedRoads);
 		}
 
 		// sort the roads to ensure that the order of roads is constant for two runs
-		Collections.sort(this.roads, new Comparator<ConvertedWay>() {
+		Collections.sort(result, new Comparator<ConvertedWay>() {
 			public int compare(ConvertedWay o1, ConvertedWay o2) {
 				return Integer.compare(o1.getIndex(), o2.getIndex());
 			}
 		});
 		
-		// copy the roads to the resulting lists
-		resultingWays.addAll(roads);
-		
 		// print out some statistics
-		int noRoadsAfterMerge = this.roads.size();
+		int noRoadsAfterMerge = result.size();
 		log.info("Roads before/after merge:", noRoadsBeforeMerge, "/",
 				noRoadsAfterMerge);
 		int percentage = (int) Math.round((noRoadsBeforeMerge - noRoadsAfterMerge) * 100.0d
 						/ noRoadsBeforeMerge);
 		log.info("Road network reduced by", percentage, "%",noMerges,"merges");
+		return result;
 	}
 
 	/**

commit 3d85f2547504a325cf4a028f662167729757ca69

fix severe error in overlays handling introduced with r3227
Styles using a an overlays file did no longer work.

git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@3246 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/mkgmap/osmstyle/ConvertedWay.java
@@ -16,7 +16,9 @@ package uk.me.parabola.mkgmap.osmstyle;
 import java.util.List;
 
 import uk.me.parabola.imgfmt.app.Coord;
+import uk.me.parabola.imgfmt.app.trergn.MapObject;
 import uk.me.parabola.log.Logger;
+import uk.me.parabola.mkgmap.general.MapElement;
 import uk.me.parabola.mkgmap.reader.osm.Element;
 import uk.me.parabola.mkgmap.reader.osm.GType;
 import uk.me.parabola.mkgmap.reader.osm.Way;
@@ -48,8 +50,8 @@ public class ConvertedWay {
 		this.index = index;
 		this.way = way;
 		this.gt = type;
-		this.isRoad = type.isRoad();
-		if (type.isRoad() & GType.isRoutableLineType(type.getType())) {
+		// note that the gt.getType() may not be a routable type when overlays are used
+		if (type.isRoad() && MapObject.hasExtendedType(gt.getType()) == false) {
 			this.roadClass = (byte) gt.getRoadClass();
 			this.roadSpeed = (byte) gt.getRoadSpeed();
 			recalcRoadClass(way);
@@ -62,6 +64,7 @@ public class ConvertedWay {
 			roadSpeed = 0;
 			mkgmapAccess = 0;
 			routeFlags = 0;
+			isRoad = false;
 		}
 	}
 	

src/uk/me/parabola/mkgmap/osmstyle/StyledConverter.java
@@ -130,10 +130,20 @@ public class StyledConverter implements OsmConverter {
 	private final boolean mergeRoads;
 	private WrongAngleFixer wrongAngleFixer;
 
+	private boolean wrongTypeMsgPrinted = false;
 	private LineAdder lineAdder = new LineAdder() {
 		public void add(MapLine element) {
-			if (element instanceof MapRoad)
+			if (element instanceof MapRoad){
+				if (GType.isRoutableLineType(element.getType()) == false){
+					if (wrongTypeMsgPrinted == false){
+						log.error("Non-routable type", GType.formatType(element.getType()), 
+								"in combination with road_class/road_speed used for routable map.",
+								"Try --check-styles to check the style.");
+						wrongTypeMsgPrinted = true;
+					}
+				}
 				collector.addRoad((MapRoad) element);
+			}
 			else
 				collector.addLine(element);
 		}
@@ -247,7 +257,6 @@ public class StyledConverter implements OsmConverter {
 	}
 
 	private int lineIndex = 0;
-	private boolean wrongTypeMsgPrinted = false;
 	private void addConvertedWay(Way way, GType foundType) {
 		if (foundType.getFeatureKind() == FeatureKind.POLYGON){ 
 			addShape(way, foundType);
@@ -281,15 +290,6 @@ public class StyledConverter implements OsmConverter {
 		}
 		else 
 			lines.add(cw);
-
-		// old code allowed all non-extended types for roads. Now print error message in this case.
-		if(foundType.isRoad() && !cw.isRoad()){
-			if (wrongTypeMsgPrinted == false){
-				log.error("Non-routable type in combination with road_class/road_speed used for routable map.",
-						"Try --check-styles to check the style.");
-
-			}
-		}
 	}
 
 	/** One type result for nodes to avoid recreating one for each node. */ 

src/uk/me/parabola/mkgmap/osmstyle/TypeReader.java
@@ -112,7 +112,7 @@ public class TypeReader {
 			}
 			if (usedTypes == null)
 				usedTypes = Arrays.asList(gt.getType());
-			
+			boolean foundRoutableType = false;
 			for (int i = 0; i < usedTypes.size(); i++){
 				int usedType = usedTypes.get(i);
 				String typeOverlaidMsg = ". Type is overlaid with " + GType.formatType(usedType);
@@ -124,6 +124,7 @@ public class TypeReader {
 				}
 				if (kind == FeatureKind.POLYLINE && gt.getMinLevel() == 0 && gt.getMaxLevel() >= 0){ 
 					if (GType.isRoutableLineType(usedType)){
+						foundRoutableType = true;
 						if (gt.isRoad() == false){
 							String msg = "Warning: routable type " + type  + " is used for non-routable line with level 0. This may break routing. Style file "+ ts.getFileName() + ", line " + ts.getLinenumber();
 							if (fromOverlays)
@@ -137,16 +138,17 @@ public class TypeReader {
 									" which is used for adding the non-routable copy of the way.");
 						}
 					} else {
-						if (gt.isRoad()){
-							String msg = "Warning: non-routable type " + type  + " is used in combination with road_class/road_speed. Line will not be routable. Style file "+ ts.getFileName() + ", line " + ts.getLinenumber();
-							if (fromOverlays)
-								msg += typeOverlaidMsg;
-							System.out.println(msg);
-						}
 						
 					}
 				}
 			}
+			if (gt.isRoad() && foundRoutableType == false){
+				String msg = "Warning: non-routable type " + type  + " is used in combination with road_class/road_speed. Line will not be routable. Style file "+ ts.getFileName() + ", line " + ts.getLinenumber();
+				if (fromOverlays)
+					msg += ". Type is overlaid, but not with a routable type";
+				System.out.println(msg);
+			}
+
 		}
 		
 		return gt;