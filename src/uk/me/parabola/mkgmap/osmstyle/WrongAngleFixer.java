openstreetmap/mkgmap

commit 1deaaa952bb897bcb14ab44a08adc756de5cb0bb

Fix issue with some barriers which are ignored.
git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@3330 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/mkgmap/osmstyle/WrongAngleFixer.java
@@ -102,9 +102,14 @@ public class WrongAngleFixer {
 			replacement.setOnBoundary(true);
 		}
 		toRepl.setReplaced(true);
-		if (toRepl instanceof CoordPOI && ((CoordPOI) toRepl).isUsed()) {
-			replacement = new CoordPOI(replacement);
-			((CoordPOI) replacement).setNode(((CoordPOI) toRepl).getNode());
+		if (toRepl instanceof CoordPOI) {
+			CoordPOI cp = (CoordPOI) toRepl;
+			if (cp.isUsed()){
+				replacement = new CoordPOI(replacement);
+				((CoordPOI) replacement).setNode(((CoordPOI) toRepl).getNode());
+				((CoordPOI) replacement).setUsed(true);
+				((CoordPOI) replacement).setConvertToViaInRouteRestriction(cp.getConvertToViaInRouteRestriction());
+			}
 		}
 		if (toRepl.isViaNodeOfRestriction())
 			replacement.setViaNodeOfRestriction(true);

src/uk/me/parabola/mkgmap/osmstyle/StyledConverter.java
@@ -647,6 +647,8 @@ public class StyledConverter implements OsmConverter {
 					if (p0 instanceof CoordPOI){
 						pNew = new CoordPOI(p0);
 						((CoordPOI) pNew).setNode(((CoordPOI) p0).getNode());
+						((CoordPOI) pNew).setUsed(((CoordPOI) p0).isUsed());
+						((CoordPOI) pNew).setConvertToViaInRouteRestriction(((CoordPOI)p0).getConvertToViaInRouteRestriction());
 					}
 					else
 						pNew = new Coord(p0);

commit 4953ff60a4b2d07ad943c909da765e022d838f46

fix error "Did not find CoordPOI node at ..." introduced with r3330
Todo: Prefer to move barrier nodes away from a junction instead of
merging with the junction node.

git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@3332 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/mkgmap/osmstyle/WrongAngleFixer.java
@@ -106,9 +106,12 @@ public class WrongAngleFixer {
 			CoordPOI cp = (CoordPOI) toRepl;
 			if (cp.isUsed()){
 				replacement = new CoordPOI(replacement);
-				((CoordPOI) replacement).setNode(((CoordPOI) toRepl).getNode());
+				((CoordPOI) replacement).setNode(cp.getNode());
 				((CoordPOI) replacement).setUsed(true);
 				((CoordPOI) replacement).setConvertToViaInRouteRestriction(cp.getConvertToViaInRouteRestriction());
+				if (replacement.highPrecEquals(cp.getNode().getLocation()) == false){
+					log.error("CoordPOI node is replaced with non-equal coordinates at", toRepl.toOSMURL());
+				}
 			}
 		}
 		if (toRepl.isViaNodeOfRestriction())
@@ -1020,11 +1023,15 @@ public class WrongAngleFixer {
 			}
 			if (c.isViaNodeOfRestriction() && (n.isViaNodeOfRestriction() || n.getOnBoundary()))
 				 return false;
-			 
+			if (c instanceof CoordPOI && (n instanceof CoordPOI || n.getOnBoundary()))
+				return false;
+			if (n instanceof CoordPOI && (c instanceof CoordPOI || c.getOnBoundary()))
+				return false;
+
 			Coord mergePoint;
-			if (c.getOnBoundary())
+			if (c.getOnBoundary() || c instanceof CoordPOI)
 				mergePoint = c;
-			else if (n.getOnBoundary())
+			else if (n.getOnBoundary() || n instanceof CoordPOI)
 				mergePoint = n;
 			else if (c.equals(n))
 				mergePoint = c;