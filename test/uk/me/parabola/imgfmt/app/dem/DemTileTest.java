760846b6382bf8facc1f8e19070536659541a5c4
gerd <gerd@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2018-01-15 10:14:31 +0000

    add some unit tests for special cases
    git-svn-id: http://svn.mkgmap.org.uk/mkgmap/branches/dem-tdb@4065 25d90789-57f7-4ee0-8453-03a3dfeeeb22

--- test/uk/me/parabola/imgfmt/app/dem/DemTileTest.java
+++ test/uk/me/parabola/imgfmt/app/dem/DemTileTest.java
@@ -18,6 +18,8 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
+import uk.me.parabola.mkgmap.reader.hgt.HGTReader;
+
 /**
  * Test some inputs for class DemTile.
  * @author Gerd Petermann
@@ -27,12 +29,51 @@ public class DemTileTest {
 	
 	@Test
 	public void testKnownBitstream() throws Exception {
+		// Example from Dem-Daten.pdf by Frank Stinner
 		short[] realHeights = new short[64*64];
 		realHeights[63 * 64] = 3;
 		DEMTile dt = new DEMTile(0, 0, 64,64, realHeights);
 		int bsLen = dt.getBitStreamLen();
 		byte[] exprectedRes = {(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xC0, 0x2e};
 		assertEquals(12, bsLen);
+		String res = Arrays.toString(dt.getBitStream());
+		assertEquals(Arrays.toString(exprectedRes), res); 
+	}	
+	
+	@Test
+	public void testAllZero() throws Exception {
+		short[] realHeights = new short[64*64];
+		DEMTile dt = new DEMTile(0, 0, 64,64, realHeights);
+		int bsLen = dt.getBitStreamLen();
+		assertEquals(0, bsLen);
+		assertEquals(0, dt.getBaseHeight());
+		assertEquals(0, dt.getMaxDeltaHeight());
+		assertEquals(0, dt.getEncodingType());
+	}	
+	
+	@Test
+	public void testAllOne() throws Exception {
+		short[] realHeights = new short[64*64];
+		Arrays.fill(realHeights, (short)1);
+		DEMTile dt = new DEMTile(0, 0, 64,64, realHeights);
+		int bsLen = dt.getBitStreamLen();
+		assertEquals(0, bsLen);
+		assertEquals(1, dt.getBaseHeight());
+		assertEquals(0, dt.getMaxDeltaHeight());
+		assertEquals(0, dt.getEncodingType());
+	}	
+
+	@Test
+	public void testAllZeroOneUndef() throws Exception {
+		short[] realHeights = new short[64*64];
+		realHeights[63 * 64] = HGTReader.UNDEF;
+		byte[] exprectedRes = {(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xC0, 0x3e};
+		DEMTile dt = new DEMTile(0, 0, 64,64, realHeights);
+		int bsLen = dt.getBitStreamLen();
+		assertEquals(12, bsLen);
+		assertEquals(0, dt.getBaseHeight());
+		assertEquals(1, dt.getMaxDeltaHeight());
+		assertEquals(2, dt.getEncodingType());
 		String res = Arrays.toString(dt.getBitStream());
 		assertEquals(Arrays.toString(exprectedRes), res); 
 	}	
